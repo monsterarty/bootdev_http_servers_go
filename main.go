dc93ed3b12a455254723feb8fc90eaba08ffdda2
monsterarty <masleanco@gmail.com>
2026-07-28 09:44:18 +0200

    Added postgre, goose, sqlc, internals, sql files

--- main.go
+++ main.go
@@ -1,21 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/monsterarty/bootdev_http_servers_go/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	database       *database.Queries
 }
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dbURL := os.Getenv("DB_URL")
+
 	const port = "8080"
 	const root = "."
 	const address = "127.0.0.1"
 
-	cfg := &apiConfig{}
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error connecting to DB: %v", err)
+	}
+	dbQueries := database.New(db)
+
+	cfg := &apiConfig{
+		database: dbQueries,
+	}
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(root)))))
